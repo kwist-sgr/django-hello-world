20e26fc268774f2408ff5cb0460ba9239a6aba08
kwist-sgr <velnik@gmail.com>
2013-04-26 14:58:41 +0300

    ticket:6 прикрутил jQuery UI Datepicker

--- django_hello_world/hello/forms.py
+++ django_hello_world/hello/forms.py
@@ -1,11 +1,18 @@
-#from django.forms import ModelForm
-from django.forms import ModelForm
-#from django.http import HttpResponseForbidden
+from django.forms import ModelForm, DateField, widgets
 
 from django_hello_world.hello.models import Profile
 
 
+class CalendarWidget(widgets.TextInput):
+
+    class Media:
+        css = {'all': "/css/jquery-ui.css"}
+        js = ("/js/jquery-ui.js")
+
+
 class ProfileEditForm(ModelForm):
+
+    #birthday = DateField(widget=CalendarWidget)
 
     class Meta:
         model = Profile
